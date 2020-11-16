12d219b3eb8d56116c03afba2111064ee7c64beb
Théo Taffin <theo.taffin@gmail.com>
2020-11-16 19:14:16 +0100

    /

--- TP2/src/TP_Unsupervised.py
+++ TP2/src/TP_Unsupervised.py
@@ -1,14 +1,14 @@
 from TP2.src.data_io import stl_load_stl10
 
+# Data manipulation library
 import numpy as np
+import pandas as pd
 
 # Data visualization library
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
 # Native libraries
 import os
-import random
 import gc
 
 # sklearn
@@ -17,12 +17,15 @@ from sklearn.linear_model import LogisticRegression
 # Tensorflow libraries
 from tensorflow.keras import layers
 from tensorflow.keras import models
-from tensorflow.keras.callbacks import TensorBoard
 
+# to avoid tf crashes
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
+# local variable
 stl10_folder = "C:/Users/Theo/PycharmProjects/BMDATA/TP2/data"
 root_folder = "C:/Users/Theo/PycharmProjects/"
+
+# data extraction
 X_train, y_train, X_test, y_test = stl_load_stl10(stl10_folder)
 
 
@@ -74,7 +77,7 @@ conv1 = layers.Conv2D(filters=128, kernel_size=(5, 5), input_shape=(96, 96, 3),
                       activation='sigmoid', weights=[conv1_weights, conv1_bias])
 maxpool1 = layers.MaxPooling2D(pool_size=(2, 2))
 
-conv2 = layers.Conv2D(filters=128, kernel_size=(5, 5), input_shape=(96, 96, 3),
+conv2 = layers.Conv2D(filters=128, kernel_size=(5, 5),
                       activation='sigmoid', weights=[conv2_weights, conv2_bias])
 maxpool2 = layers.MaxPooling2D(pool_size=(2, 2))
 
@@ -170,4 +173,11 @@ for max_iter in range(1000, 10000, 1000):
     lst_train_score.append(train_score)
     lst_test_score.append(test_score)
 
-plt.plot()
+score_df = pd.DataFrame.from_dict({'x': x,
+                                   'train_score': lst_train_score,
+                                   'test_score': lst_test_score}).set_index('x')
+plt.plot(score_df)
+plt.xlabel('iterations')
+plt.ylabel('score')
+plt.legend(['train_score', 'test_score'])
+plt.show()
