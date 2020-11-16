0adc9445586530549eed135cb384d5901e69fa5c
Théo Taffin <theo.taffin@gmail.com>
2020-11-16 15:49:54 +0100

    /

--- TP2/src/TP_Unsupervised.py
+++ TP2/src/TP_Unsupervised.py
@@ -1,5 +1,7 @@
 from TP2.src.data_io import stl_load_stl10
 
+import numpy as np
+
 # Data visualization library
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -16,6 +18,74 @@ from tensorflow.keras.callbacks import TensorBoard
 
 
 stl10_folder = "C:/Users/Theo/PycharmProjects/BMDATA/TP2/data"
+root_folder = "C:/Users/Theo/PycharmProjects/"
 X_train, y_train, X_test, y_test = stl_load_stl10(stl10_folder)
 
 
+# normalize 0 - 1
+def normalize_0_1(images):
+
+    tmp = np.zeros(shape=(len(images), 96, 96, 3), dtype=np.uint8)
+
+    for count, image in enumerate(images):
+        nor = image / image.max()
+        tmp[count] = nor
+
+    return tmp
+
+
+X_train_n = (X_train/255)
+X_test_n = (X_test/255)
+
+del X_train
+del X_test
+gc.collect()
+
+mean_x_train = X_train_n.mean()
+std_x_train = X_train_n.std()
+
+X_train_c = (X_train_n - mean_x_train) / std_x_train
+X_test_c = (X_test_n - mean_x_train) / std_x_train
+
+del X_train_n
+del X_test_n
+gc.collect()
+
+mini = X_train_c.min()
+maxi = X_train_c.max() - mini
+
+X_train_c_r_n = (X_train_c - mini) / maxi
+X_test_c_r_n = (X_test_c - mini) / maxi
+
+del X_train_c
+del X_test_c
+gc.collect()
+
+conv1_weights = np.load(os.path.join(root_folder, "BMDATA/TP2/model/conv1_weights.npy"))
+conv1_bias = np.load(os.path.join(root_folder, "BMDATA/TP2/model/conv1_bias.npy"))
+
+conv2_weights = np.load(os.path.join(root_folder, "BMDATA/TP2/model/conv2_weights.npy"))
+conv2_bias = np.load(os.path.join(root_folder, "BMDATA/TP2/model/conv2_bias.npy"))
+
+tr_conv1_weights = np.load(os.path.join(root_folder, "BMDATA/TP2/model/tr_conv1_weights.npy"))
+tr_conv1_bias = np.load(os.path.join(root_folder, "BMDATA/TP2/model/tr_conv1_bias.npy"))
+
+tr_conv2_weights = np.load(os.path.join(root_folder, "BMDATA/TP2/model/tr_conv2_weights.npy"))
+tr_conv2_bias = np.load(os.path.join(root_folder, "BMDATA/TP2/model/tr_conv2_bias.npy"))
+
+conv1 = layers.Conv2D(filters=128, kernel_size=(5, 5), input_shape=(96, 96, 3),
+                      activation='sigmoid', weights=[conv1_weights, conv1_bias])
+maxpool1 = layers.MaxPooling2D(pool_size=(2, 2))
+
+conv2 = layers.Conv2D(filters=128, kernel_size=(5, 5), input_shape=(96, 96, 3),
+                      weights=[conv2_weights, conv2_bias], activation='sigmoid')
+maxpool2 = layers.MaxPooling2D(pool_size=(2, 2))
+
+tr_conv1 = layers.Conv2DTranspose(filters=128, kernel_size=(5, 5), strides=(2, 2), padding='same',
+                                  weights=[tr_conv1_weights, tr_conv1_bias])
+tr_conv2 = layers.Conv2DTranspose(filters=3, kernel_size=(5, 5), strides=(2, 2), padding='same',
+                                  weights=[tr_conv2_weights, tr_conv2_bias])
+
+model = models.Sequential([conv1, maxpool1, conv2, maxpool2, tr_conv1, tr_conv2])
+model.compile()
+model.summary()
